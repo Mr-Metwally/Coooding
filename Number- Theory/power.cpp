ce5e06086484e96af70e5b0380600f80b7608a5f
Mostafa Metwally <memometo.777@gmail.com>
2026-03-15 15:20:24 +0200

    Add modular exponentiation function in power.cpp
    Implemented modular exponentiation function to handle large powers efficiently.

--- Number- Theory/power.cpp
+++ Number- Theory/power.cpp
@@ -23,6 +23,38 @@ int main()
     return 0;
 }
 
+                // with large powers, but taking the modulus of them.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const ll mod=10e9+7;
+ll powermod(ll a, ll b)
+{
+    if(b==0) return 1;
+    else if(b==1) return a;
+
+    ll half = powermod(a,b/2) % mod;
+
+    if(!b&1) return (half*half) % mod;
+
+    else return (a*half*half) % mod;
+}
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout<<powermod(5,1231324263456234532)<<'\n';
+    cout<<pow(5,3);
+    return 0;
+}
+
+
+
+
+
 
 
 
