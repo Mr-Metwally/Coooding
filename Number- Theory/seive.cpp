5fcd151a38cec7d969fe32fd251db7b26afbeb28
Mostafa Metwally <memometo.777@gmail.com>
2026-03-18 03:59:08 +0200

    Add linear sieve function for prime generation
    Implemented linear sieve algorithm for prime number generation.

--- Number- Theory/seive.cpp
+++ Number- Theory/seive.cpp
@@ -28,3 +28,37 @@ int main()
 
     return 0;
 }
+//---------------------------------------------Linear_seive----------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int N=1e5+5;
+vector<bool>isPrime (N,1); vector<int> primes;
+void linear_seive()
+{
+    isPrime[0]=isPrime[1]=0;
+    for(ll i=2;i<=N;i++)
+    {
+        if(isPrime[i]) primes.push_back(i);
+        for(auto it:primes)
+        {
+            if(i*it>=N) break;
+            isPrime[i*it]==0;
+
+            if(i%it==0) break;
+        }
+    }
+}
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+  
+    return 0;
+}
+
+
+
+
