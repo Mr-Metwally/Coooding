3d1b7ecfae80b290f07e5ddddb9069b9ef5ca2f5
Mostafa Metwally <memometo.777@gmail.com>
2026-03-23 23:56:20 +0200

    Update apple division.cpp

--- CSES p-set/apple division.cpp
+++ CSES p-set/apple division.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define ll long long
 
+// this code solve a classic meninum difference problem or partition problem.
 int p[20];
 int main() 
 {
@@ -11,19 +12,19 @@ int main()
     int n;
     cin>>n;
     ll s=0, ans=0;
-    for(int i=0;i<n;i++) cin>>p[i], s+=p[i];
+    for(int i=0;i<n;i++) cin>>p[i], s+=p[i];  // store the largest sum of a subset >= s/2.
     
-    for(int i=0;i<(1<<n);i++)
+    for(int i=0;i<(1<<n);i++)   // for loop on all subsets in binary (this gives the total number of subsets).
     {
         ll sum=0;
-        for(int j=0;j<n;j++) if(i>>j&1) sum+=p[i];
+        for(int j=0;j<n;j++) if(i>>j&1) sum+=p[j]; // if statment checks if j-th element is included in the subset
 
-        if(sum<=s/2)
+        if(sum<=s/2)   // we want the sum of the subset at closer as possible to the half sum. this minimizes the difference
         {
             ans=max(ans,sum);
         }
     }
-    cout<<s-2*ans<<endl;    
-
+    cout<<s-2*ans<<endl;    // if one subset has a sum of ans, then the other subset has a sum of s-ans. 
+                            // when subtracting them----> (s-ans)-ans = s-2*ans.
     return 0;
 }
