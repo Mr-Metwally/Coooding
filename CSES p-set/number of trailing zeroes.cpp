b99585f375ff157ddf34a3c31595a667b9c84dfe
Mostafa Metwally <memometo.777@gmail.com>
2026-03-23 22:39:19 +0200

    Create number of trailing zeroes.cpp

---
+++ CSES p-set/number of trailing zeroes.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll n; cin>>n;
+    ll ans=0;
+    for(int i=5;i<=n;i*=5)
+    {
+        ans+=n/i;
+    }
+    cout<<ans<<"\n";
+
+    
+    return 0;
+}
